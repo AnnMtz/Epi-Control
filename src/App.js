321d1936a187167a4eb580813c51cb7f6f184651
MonMtz <david@vkapp.mx>
2019-07-13 22:44:08 -0500

    subiendo cambios en vistas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import Login from './components/Login/index';
+import Login from './components/Login/index';
 // import Register from './components/Register/index';
 // import Recovery from './components/Recovery/index';
 // import Newpassword from './components/Newpassword/index';
@@ -10,20 +10,20 @@ import './App.css';
 // import TableRegister from './components/TableRegister';
 // import ScheduleAppointment from './components/ScheduleAppointment';
 // import MedicalRecord from './components/MedicalRecord';
-import EmergencyContacts from './components/EmergencyContacts';
+// import EmergencyContacts from './components/EmergencyContacts';
 // import InformationSaved from './components/InformationSaved';
 // import RegisterTables from './components/RegisterTables';
 
 function App() {
   return (
     <div className="App">
-      {/* <Login></Login> */}
+      <Login></Login>
       {/* <Register></Register> */}
       {/* <Recovery></Recovery> */}
       {/* <Newpassword></Newpassword> */}
       {/* <Home></Home> */}
       {/* <PersonalInformation></PersonalInformation> */}
-      <EmergencyContacts></EmergencyContacts>
+      {/* <EmergencyContacts></EmergencyContacts> */}
       {/* <InformationSaved></InformationSaved> */}
       {/* <MedicalRecord></MedicalRecord> */}
       {/* <ScheduleAppointment></ScheduleAppointment> */}
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,29 +1,43 @@
-import React from 'react';
+import React,{Component} from 'react';
 import Logo from '../../Assets/img/logo.png';
 import './Login.scss';
 
-const Login = () => (
-    <div className ="MainContainer">
-        <div className ="ContainerLogin">
-                        <div className="col-sm-6 LoginMain">
-                        <h1 className="LoginTitle">¡BIENVENID@!</h1>
-                            <div className="logo">
-                                <img src={Logo} alt="logo"/>
-                            </div>
-                            <p>Tu mejor compañia en el seguimiento de tu epilepsía</p>
-                        </div>
-                        <div className="col-sm-6">
-                            <h1 className="LoginTitle">Iniciar Sesión</h1>
-                            <label className="UserLabel" htmlFor="user">Usuario</label>
-                            <input className="InputLogin" type="text" placeholder="Usuario" id="user"/>
-                            <label className="UserLabel" htmlFor="password">Contraseña</label>
-                            <input className="InputLogin" type="password" placeholder="Contraseña" id="password"/>
-                            <a className="LinkPassword" href="#">¿Olvidaste tu contraseña</a>
-                            <button className="BtnLogin" type="button">Enviar</button>
-                        </div>
+class Login extends Component{
+
+    constructor (props) {
+        super(props);
+        this.clickLogin = this.clickLogin.bind(this);
+    };
+    
+    clickLogin = () => {
+        console.log('ya entramos');
+        
+    }
+    render(){
+        return(
+            <div className ="MainContainer">
+                <div className ="ContainerLogin">
+                                <div className="col-sm-6 LoginMain">
+                                <h1 className="LoginTitle">¡BIENVENID@!</h1>
+                                    <div className="logo">
+                                        <img src={Logo} alt="logo"/>
+                                    </div>
+                                    <p>Tu mejor compañia en el seguimiento de tu epilepsía</p>
+                                </div>
+                                <div className="col-sm-6">
+                                    <h1 className="LoginTitle">Iniciar Sesión</h1>
+                                    <label className="UserLabel" htmlFor="user">Usuario</label>
+                                    <input className="InputLogin" type="text" placeholder="Usuario" id="user"/>
+                                    <label className="UserLabel" htmlFor="password">Contraseña</label>
+                                    <input className="InputLogin" type="password" placeholder="Contraseña" id="password"/>
+                                    <a className="LinkPassword" href="#">¿Olvidaste tu contraseña</a>
+                                    <button className="BtnLogin" type="button" onClick={this.clickLogin}>Enviar</button>
+                                </div>
+                    </div>
             </div>
-    </div>
-)
+        )
+    }
+} 
 
 
 export default Login;
