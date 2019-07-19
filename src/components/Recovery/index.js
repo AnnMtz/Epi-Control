585c73621df5767f6997f9aa5d678a13aeedc201
Daniel Ortega Escandón <dev.escandon@gmail.com>
2019-07-19 00:55:30 -0500

    Modificacion de las vistas de login y registrarse

--- src/components/Recovery/index.js
+++ src/components/Recovery/index.js
@@ -4,24 +4,28 @@ import Return from '../../Assets/img/return.png';
 import './Recovery.scss';
 
 const Recovery = () => (
-    <div className = 'MainContainer'>
-        <div className ="ContainerLogin">
-                <div className="LoginMain">
-                <h1 className="LoginTitle">Lepsi</h1>
-                    <h3 className="LoginSubtitle">¿Sabías que?</h3>
-                    <p className="LoginParagraph">Tener convulsiones en momentos determinados puede llevar a circunstancias peligrosas para ti o para otras personas.</p>
+    <div className = 'MainContainerRecovery'>
+        <div className ="ContainerRecovery">
+                <div className="RecoveryMain">
+                <h1 className="RecoveryTitle">Lepsi</h1>
+                    <h3 className="RecoverySubtitle">¿Sabías que?</h3>
+                    <p className="RecoveryParagraph">Tener convulsiones en momentos determinados puede llevar a circunstancias peligrosas para ti o para otras personas.</p>
                 </div>
                 <div className="col-sm-6">
                     <h1 className="WelcomeTitle">Restablecer contraseña</h1>
-                    <label className="UserLabel" htmlFor="password">Contraseña nueva</label>
-                    <input className="InputLogin" type="text" id="password"/>
-                    <label className="UserLabel" htmlFor="password">Confirmar contraseña</label>
-                    <input className="InputLogin" type="password" id="password"/>
+                    <div className="InputContainer">
+                        <label className="UserLabel" htmlFor="password">Contraseña nueva</label>
+                        <input className="InputRecovery" type="text" id="password"/>
+                    </div>
+                    <div className="InputContainer">
+                        <label className="UserLabel" htmlFor="password">Confirmar contraseña</label>
+                        <input className="InputRecovery" type="password" id="password"/>
+                    </div>
                     {/* <label className="UserLabel" htmlFor="password">Contraseña</label>
-                    <input className="InputLogin" type="password" id="password"/> */}
+                    <input className="InputRecovery" type="password" id="password"/> */}
                     {/* <label className="UserLabel" htmlFor="password2">Confirma tu contraseña</label>
-                    <input className="InputLogin" type="password" id="password"/> */}
-                    <button className="BtnLogin" type="button">Restablecer contraseña</button>
+                    <input className="InputRecovery" type="password" id="password"/> */}
+                    <button className="BtnRecovery" type="button">Restablecer contraseña</button>
                 </div>
         </div>
     </div>
