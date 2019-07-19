29170be9ce349c841f2dae6b8e3866f91eab41b5
Daniel Ortega Escandón <dev.escandon@gmail.com>
2019-07-19 06:20:35 -0500

    Modificacion de la vista de home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,32 +1,32 @@
 import React from 'react';
 import './Home.scss';
 import Header from '../Header';
-import { Link } from 'react-router-dom';
+import Consult from "../../Assets/img/consult.jpg"
 
 const Home = () => (
     <div className ='MainContainer'>
         <div className ='ContainerHome'>
-        <Header></Header>
-            <div className="HomeInfoContainer">
-                <div className="AlertButton">
-                    <button type="submit" className="Alert">Alerta</button>
+            <Header></Header>
+            <div className="PrincipalInfoContainer">
+                <div className="TipsContainer">
+                    <span id="TitleTips">TIPS</span>
+                    <div id="InfoTipsContainer">
+                        <div className="ImageContainer">
+                            <img id="ImageDoctor" alt="Imagen de tips" src={Consult}/>
+                        </div>
+                        <p id="InfoParagraphContaier">
+                            Intenta ignorar las reacciones negativas de la gente. 
+                            Es útil aprender sobre la epilepsia para que sepas cuál es la 
+                            información verdadera, en lugar de tener ideas equivocadas 
+                            sobre la enfermedad. Y trata de mantener tu sentido del humor.
+                        </p>
+                    </div>
+                </div>        
+                <div className="HomeInfoContainer">
+                    <div className="AlertButton">
+                        <button type="submit" className="Alert">Alerta</button>
+                    </div>
                 </div>
-                {/* <h3 className="Home">Ingresa nueva una nueva contraseña.</h3> */}
-                {/* <form className="FormHome">
-                    <div className="InfoContainer">
-                        <label className="Email">Correo <br/>
-                            <input className="EmailInput" placeholder="Ingresa tu correo"></input>
-                        </label>
-                    </div>
-                    <div className="InfoContainer">
-                        <label className="ConfirmEmail">Confirma tu correo <br/>
-                            <input className="ConfirmEmailInput" placeholder="Confirma tu correo"></input>
-                        </label>
-                    </div>
-                    {/* <div className="ButtonHomeContainer">
-                        <button className="ButtonHome">Enviar</button>
-                    </div> 
-                </form> */}
             </div>
         </div>
     </div>
